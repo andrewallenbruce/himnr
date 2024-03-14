1e4f586faecb1917abbb02515462906f3049839e
Andrew Bruce <andrewbruce.himni@gmail.com>
2024-03-14 13:53:28 -0700

    acronyms

--- setup-doc.R
+++ setup-doc.R
@@ -9,6 +9,6 @@ knitr::opts_chunk$set(
   dev       = "ragg_png",
   out.width = "100%",
   fig.align = "center",
-  fig.width = 8,
-  fig.asp   = 0.618 # 1 / phi
+  # fig.asp   = 0.618 # 1 / phi
+  fig.width = 8
 )
