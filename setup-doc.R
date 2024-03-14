a912b6219254d30dba3bcaa6ef1dc905494f9a66
Andrew Bruce <andrewbruce.himni@gmail.com>
2024-03-14 06:00:49 -0700

    acronyms, fontawesome extensions

---
+++ setup-doc.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(downlit)
+library(gt)
+library(northstar)
+library(provider)
+
+knitr::opts_chunk$set(
+  comment   = "#>",
+  dev       = "ragg_png",
+  out.width = "100%",
+  fig.align = "center",
+  fig.width = 8,
+  fig.asp   = 0.618 # 1 / phi
+)
