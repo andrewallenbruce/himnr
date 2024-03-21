0917d769c24dc3d01c24fcf518d88cf4b10d9cf9
Andrew Bruce <andrewbruce.himni@gmail.com>
2024-03-21 16:50:16 -0700

    removed acr extension

--- setup-doc.R
+++ setup-doc.R
@@ -12,3 +12,45 @@ knitr::opts_chunk$set(
   # fig.asp   = 0.618 # 1 / phi
   fig.width = 8
 )
+
+gt_style <- function(gt_tbl) {
+  gt_tbl |> 
+    # opt_all_caps() |>
+    fmt_markdown() |> 
+    fmt_integer() |> 
+    cols_align("left") |> 
+    tab_style(
+      style = cell_text(align = "center", 
+                        size = px(18),
+                        weight = "bold"),
+      locations = cells_stub()) |> 
+    tab_options(
+      quarto.disable_processing = TRUE,
+      column_labels.hidden       = TRUE,
+      table.font.size            = px(14),
+      table.width                = pct(100),
+      heading.align = "left",
+      heading.title.font.size    = px(16),
+      heading.subtitle.font.size = px(16),
+      source_notes.font.size     = px(16),
+      row_group.as_column        = TRUE,
+      row_group.font.size        = px(24)
+    )
+}
+
+gt_marks <- function(gt_tbl, cols) {
+  
+  gt_tbl |>
+    gt::text_case_when(
+      x == TRUE ~ gt::html(
+        fontawesome::fa("check",
+                        prefer_type = "solid",
+                        fill = "red")),
+      x == FALSE ~ gt::html(
+        fontawesome::fa("xmark",
+                        prefer_type = "solid",
+                        fill = "white")),
+      .default = NA,
+      .locations = gt::cells_body(
+        columns = {{ cols }}))
+}
